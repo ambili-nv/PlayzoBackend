3ddc112e38a7007b7ff506fb6204e3c9cda315ee
ambili-nv <ambilinadvilvalappil@gmail.com>
2024-12-06 16:29:53 +0530

    updated

--- src/framework/webserver/server.ts
+++ src/framework/webserver/server.ts
@@ -1,13 +1,10 @@
 
 import configKeys from "../../config";
 import { Server } from "http";
-const PORT = process.env.PORT || 3000;
+
 const server = (app:Server)=>{
-    app.listen(PORT,()=>
-    console.log(`Server listening on http://localhost:${PORT}`)
+    app.listen(configKeys.PORT,()=>
+    console.log(`Server listening on http://localhost:${configKeys.PORT}`)
     );
-    // app.listen(configKeys.PORT,()=>
-    // console.log(`Server listening on http://localhost:${configKeys.PORT}`)
-    // );
 }
 export default server;
